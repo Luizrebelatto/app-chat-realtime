c751e21b169541c020c1b628358ee0b72b1ad0eb
Luizrebelatto <lbrebelatto@gmail.com>
2024-05-30 19:02:58 -0300

    chore: create type styled-components

--- App.tsx
+++ App.tsx
@@ -1,9 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { Routes } from './src/routes';
-
+import { ThemeProvider } from "styled-components/native"
+import theme from './src/globals/theme';
 export default function App() {
   return (
-    <Routes/>
+    <ThemeProvider theme={theme}>
+      <Routes/>
+    </ThemeProvider>
   );
 }
